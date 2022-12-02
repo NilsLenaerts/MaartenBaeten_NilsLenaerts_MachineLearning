2b5900e4f901a5fbbb5760f3b39e99f4a728c638
Nils Lenaerts <nils.lenaerts00@gmail.com>
2022-12-02 17:43:43 +0100

    Tensorflow complete

--- Neural_Network_Tensor.py
+++ Neural_Network_Tensor.py
@@ -1,140 +1,64 @@
-# used for manipulating directory paths
-import os
-
-# Scientific and vector computation for python
-import numpy as np
-from numpy import asarray
-from numpy import savetxt
-
-# Plotting library
-from matplotlib import pyplot
-import matplotlib.image as mpimg
-import matplotlib.cm as cm
-
-# Optimization module in scipy
-from scipy import optimize
-from scipy import misc
-
-# will be used to load MATLAB mat datafile format
-from scipy.io import loadmat
-
-# importing os module
-import os
-
-from skimage import color
-from skimage import io
-from skimage.transform import rescale, resize, downscale_local_mean
-
 import tensorflow as tf
-
-
-def sigmoid(x):
-    return 1/(1 + np.exp(-x))
-
-
-X_size = 14284  # 2046 validation 14284 train 2039 valid which are 691200 and 14284 which are 691200
-Gray_size = 57600  # 230400
-
-
-def loadData(directory_path):
-    labels_int = np.zeros((X_size))
-    gray_arrays = np.zeros((X_size, Gray_size))
-    directory_path
-    ext = ('.jpg')
-    i = 0
-    j = 0
-    for directory in os.listdir(directory_path):
-        newPath = directory_path + '/' + directory
-        for file in os.listdir(newPath):
-            if file.endswith(ext):
-                path = newPath + '/' + file
-
-                img = io.imread(path)
-                if (img.size == 691200):
-                    imgGray = color.rgb2gray(img)
-                    res_img = rescale(imgGray, 0.5, anti_aliasing=False)
-                    imgn = np.reshape(res_img, (1, Gray_size), order='F')
-                    gray_arrays[i] = imgn
-                    type = directory
-                    match type:
-                        case 'd4':
-                            labels_int[i] = 0
-                        case 'd6':
-                            labels_int[i] = 1
-                        case 'd8':
-                            labels_int[i] = 2
-                        case 'd10':
-                            labels_int[i] = 3
-                        case 'd12':
-                            labels_int[i] = 4
-                        case 'd20':
-                            labels_int[i] = 5
-                else:
-                    continue
-
-                i = i + 1
-                j = j+1
-            else:
-                continue
-    i = 0
-
-    return gray_arrays, labels_int, j
-
-
+from tensorflow import keras
+from keras import layers
+import cv2 as cv
+import loadImages
+import numpy as np
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+def unison_shuffled_copies(a, b):
+    assert len(a) == len(b)
+    p = np.random.permutation(len(a))
+    return a[p], b[p]
 def main():
     print("TensorFlow version:", tf.__version__)
+    
+    x_train, y_train, x_val, y_val = loadImages.loadTensorFlowData()
+    
+    x_train, y_train = unison_shuffled_copies(x_train, y_train)
+    x_val, y_val = unison_shuffled_copies(x_val, y_val)
+    inputs = keras.Input(shape=(60,60,1))
+    x = layers.Rescaling(1.0/255)(inputs)
+    x = layers.Conv2D(filters=32,kernel_size=(3,3), activation="relu", kernel_initializer="he_uniform", padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Conv2D(filters=32,kernel_size=(3,3), activation="relu", kernel_initializer="he_uniform", padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.MaxPooling2D(pool_size=(2,2))(x)
+    x = layers.Dropout(0.2)(x)
+    x = layers.Flatten()(x)
+    #x = layers.Dense(512, activation="relu")(x)
+    x = layers.Dense(128, activation="relu")(x)
+    outputs = layers.Dense(6, activation="softmax")(x)
+    model = keras.Model(inputs,outputs)
+    model.summary()
 
-    gray_arrays_train, labels_int_train, j_train = loadData(
-        r"./Data/dice-d4-d6-d8-d10-d12-d20/dice/train")
+    model.compile(optimizer="adam", loss = "sparse_categorical_crossentropy", metrics=[keras.metrics.SparseCategoricalAccuracy(name="acc")])
+    batch_size = 32
 
-    x_train = gray_arrays_train
-    y_train = labels_int_train
+    callbacks = [
+    keras.callbacks.ModelCheckpoint(
+        filepath='./TensorModels/model_{epoch}',
+        save_freq='epoch')
+    ]
 
-    gray_arrays_test, labels_int_test, j_test = loadData(
-        r"./Data/dice-d4-d6-d8-d10-d12-d20/dice/valid")
-    x_test = gray_arrays_test
-    y_test = labels_int_test
+    dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train)).batch(batch_size)
 
-    # Setup the parameters you will use for this exercise
-    input_layer_size = 57600  # 240x240 Input Images
-    hidden_layer_size = 10000   # 200 hidden units
-    num_labels = 6          # 6 labels
+    print("Fit on Dataset")
+    history = model.fit(dataset, epochs=10)
+    val_dataset = tf.data.Dataset.from_tensor_slices((x_val, y_val)).batch(1)
+    #history = model.fit(dataset, epochs=1, validation_data=val_dataset)
+    loss, acc = model.evaluate(val_dataset,batch_size=1)  # returns loss and metrics
+    print("loss: %.2f" % loss)
+    print("acc: %.2f" % acc)
+    model.save("./TensorModels/model_3")
+    predict = model.predict(val_dataset,batch_size=1)
+    print(y_val[0:8])
+    print(np.argmax(predict[0:8], axis=1))
+    print(y_val[880:888])
+    print(np.argmax(predict[880:888], axis=1))
+    print(y_val[0])
+    cv.imshow("random", x_val[0])
+    cv.waitKey(100000)
 
-    # Build a tf.keras.Sequential model by stacking layers.
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Flatten(input_shape=(57600, 1)),
-        tf.keras.layers.Dense(hidden_layer_size, activation='relu'),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(num_labels)
-    ])
-
-    # For each example, the model returns a vector of logits or log-odds scores, one for each class.
-    predictions = model(x_train[:1]).numpy()
-
-    # The tf.nn.softmax function converts these logits to probabilities for each class:
-    tf.nn.softmax(predictions).numpy()
-
-    # Define a loss function for training using losses.SparseCategoricalCrossentropy,
-    # which takes a vector of logits and a True index and returns a scalar loss for each example.
-    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-
-    # This loss is equal to the negative log probability of the true class: The loss is zero if the model is sure of the correct class.
-    loss_fn(y_train[:1], predictions).numpy()
-
-    # Before you start training, configure and compile the model using Keras Model.compile.
-    # Set the optimizer class to adam, set the loss to the loss_fn function you defined earlier
-    # and specify a metric to be evaluated for the model by setting the metrics parameter to accuracy.
-    model.compile(optimizer='adam', loss=loss_fn, metrics=['accuracy'])
-
-    # Use the Model.fit method to adjust your model parameters and minimize the loss:
-    model.fit(x_train, y_train, epochs=5)
-
-    # The Model.evaluate method checks the models performance, usually on a "Validation-set" or "Test-set".
-    model.evaluate(x_test,  y_test, verbose=2)
-
-    # If you want your model to return a probability, you can wrap the trained model, and attach the softmax to it:
-    probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
-    probability_model(x_test[:5])
-
-
-main()
+if __name__ == "__main__":
+    main()
--- loadImages.py
+++ loadImages.py
@@ -5,6 +5,62 @@ import os
 
 
 diceTypes = ["d4","d6","d8","d10","d12","d20"]
+
+def loadTensorFlowData():
+    # m is the amount of training data in the dataset , in this case the amount of pictures.
+    # This will be increased for every picture that is read
+    m = 0
+
+    # v is the amount of validation data in the dataset
+    # This will be increased for every picture that is read
+    v = 0
+
+    # n is the amount of features the data has, in our case it is the amount of pixels in a photo
+    # we are currently only using pictures of size 240x240
+    n = 60
+    
+    # k is the number of categories, there are 6 different types of dice
+    k = 6
+    os.chdir("Data/")
+    for root, dirs, files in os.walk('grayscale/dice/train'):
+        if files:
+            m += len(files)
+    for root, dirs, files in os.walk('grayscale/dice/valid'):
+        if files:
+            v += len(files)
+    x_train = np.zeros((m,n,n), dtype=np.uint8)
+    y_train = np.zeros((m), dtype=np.uint8)
+    x_val = np.zeros((v,n,n), dtype=np.uint8)
+    y_val = np.zeros((v), dtype=np.uint8)
+    valAmount = 0
+    trainAmount = 0
+    for i in ["valid", "train"]:
+        for j in range(len(diceTypes)):
+            inLoopVal = 0
+            inLoopTrain = 0
+            dirPath = "grayscale/dice/" + i + "/" + diceTypes[j] + "/*.jpg"
+            images = glob.glob(dirPath)
+            print("Loading images from: ",dirPath)
+            for file in images:
+                img = cv.imread(file,0)
+                if(i == "valid"):
+                    if(inLoopVal >=  5000):
+                        break
+                    x_val[valAmount] = img
+                    y_val[valAmount] = j
+                    inLoopVal +=1
+                    valAmount +=1
+                    
+                else:
+                    if(inLoopTrain >= 10000):
+                        break
+                    x_train[trainAmount] = img
+                    y_train[trainAmount] = j
+                    inLoopTrain +=1
+                    trainAmount +=1
+    return x_train,y_train, x_val, y_val
+
+
 def loadData():
     os.chdir("Data/")
     # m is the amount of training data in the dataset , in this case the amount of pictures.
