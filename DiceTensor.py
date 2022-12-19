bc0ad1321f4012c64a2af29cc8345ca2f5fbbb37
Nils Lenaerts <nils.lenaerts00@gmail.com>
2022-12-19 17:59:34 +0100

    TFLite model

---
+++ DiceTensor.py
@@ -0,0 +1,21 @@
+import numpy as np
+import loadImages
+import tensorflow as tf
+
+
+def unison_shuffled_copies(a, b):
+    assert len(a) == len(b)
+    p = np.random.permutation(len(a))
+    return a[p], b[p]
+
+def main():
+    
+    converter = tf.lite.TFLiteConverter.from_saved_model("Data/TensorModels/model_3")
+    tflite_model = converter.convert()
+
+    with open("Data/TensorModels/model.tflite","wb") as f:
+        f.write(tflite_model)
+    
+
+if __name__ == "__main__":
+    main()
